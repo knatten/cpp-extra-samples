db5157f942fd48785741c27deadc7a261a8ea726
AzureAD\SatjaSivcev <satja.sivcev@zivid.com>
2020-01-07 18:30:26 +0100

    Croping ROI

--- MaskingROI/MaskingROI.cpp
+++ MaskingROI/MaskingROI.cpp
@@ -41,16 +41,56 @@ int main()
         std::cout << "Running the visualizer. Blocking until the window closes" << std::endl;
         vis.run();
 
+		/*
+         * (0,0)
+         *  +-------------------------------------------+  x-dimension
+         *  |                                           |
+         *  |  (roiX, roiY)                             |
+         *  |    o--------------------o                 |
+         *  |    |                    |  roiHeight      | height=1200
+         *  |    o--------------------o                 |
+         *  |          roiWidth                         |
+         *  |                                           |
+         *  |                                           |
+         *  +-------------------------------------------+
+         *  y-dimension        width=1920
+         *
+         */
+
+        std::size_t roiX = 660;
+        std::size_t roiY = 600;
+        std::size_t roiWidth = 600;
+        std::size_t roiHeight = 400;
+
         std::cout << "Converting ZDF point cloud to OpenCV format" << std::endl;
 
         // Creating OpenCV structure
-        cv::Mat rgb((int)pointCloud.height(), (int)pointCloud.width(), CV_8UC3, cv::Scalar(0, 0, 0));
-        cv::Mat x((int)pointCloud.height(), (int)pointCloud.width(), CV_32F, cv::Scalar(0));
-        cv::Mat y((int)pointCloud.height(), (int)pointCloud.width(), CV_32F, cv::Scalar(0));
-        cv::Mat z((int)pointCloud.height(), (int)pointCloud.width(), CV_32F, cv::Scalar(0));
+        cv::Mat rgb((int)roiHeight, (int)roiWidth, CV_8UC3, cv::Scalar(0, 0, 0));
+        cv::Mat x((int)roiHeight, (int)roiWidth, CV_32F, cv::Scalar(0));
+        cv::Mat y((int)roiHeight, (int)roiWidth, CV_32F, cv::Scalar(0));
+        cv::Mat z((int)roiHeight, (int)roiWidth, CV_32F, cv::Scalar(0));
+
+		auto pcDataPtr = pointCloud.dataPtr();
+        //std::unique_ptr<Float3[]> array(new Float3[roiWidth * roiHeight]);
+        for(std::size_t i = 0; i < roiHeight; i++)
+        {
+            for(std::size_t j = 0; j < roiWidth; j++)
+            {
+                //auto point = pcDataPtr[(roiY + i) * pointCloud.width() + (roiX + j)];
+
+				cv::Vec3b &color = rgb.at<cv::Vec3b>(i, j);
+                color[0] = pointCloud(roiY + i, roiX + j).blue();
+                color[1] = pointCloud(roiY + i, roiX + j).green();
+                color[2] = pointCloud(roiY + i, roiX + j).red();
+                x.at<float>(i, j) = pointCloud(roiY + i, roiX + j).x;
+                y.at<float>(i, j) = pointCloud(roiY + i, roiX + j).y;
+                z.at<float>(i, j) = pointCloud(roiY + i, roiX + j).z;
+                //array[y * roiWidth + x] = Float3{ point.x, point.y, point.z };
+            }
+        }
 
         // Filling in OpenCV matrices with the cloud data
-        for(size_t i = 0; i < pointCloud.height(); i++)
+        /*for(size_t i = 0; i < pointCloud.height(); i++)
         {
             for(size_t j = 0; j < pointCloud.width(); j++)
             {
@@ -62,10 +102,10 @@ int main()
                 y.at<float>(i, j) = pointCloud(i, j).y;
                 z.at<float>(i, j) = pointCloud(i, j).z;
             }
-        }
+        }*/
 
         // Masking the point cloud
-        cv::Mat mask = cv::Mat::zeros(x.size(), CV_8U);
+        /*cv::Mat mask = cv::Mat::zeros(x.size(), CV_8U);
 
         //Circle
         //cv::circle(mask, cv::Point(mask.cols / 2, mask.rows / 2), 200, cv::Scalar(255, 0, 0), -1, 8, 0);
@@ -82,7 +122,7 @@ int main()
                       0);*/
 
         //Rectangle2
-        cv::Rect rect(660, 400, 600, 400);
+        /*cv::Rect rect(660, 400, 600, 400);
         cv::rectangle(mask, rect, cv::Scalar(255, 0, 0), -1, 8, 0);
 
         cv::Mat xMasked = cv::Mat::zeros(x.size(), x.type());
@@ -93,21 +133,21 @@ int main()
         x.copyTo(xMasked, mask);
         y.copyTo(yMasked, mask);
         z.copyTo(zMasked, mask);
-        rgb.copyTo(rgbMasked, mask);
+        rgb.copyTo(rgbMasked, mask);*/
 
         // Creating a PointCloud structure
         pcl::PointCloud<pcl::PointXYZRGB> cloud;
 
         // Filling in the cloud data
-        cloud.width = pointCloud.width();
-        cloud.height = pointCloud.height();
+        cloud.width = roiWidth;
+        cloud.height = roiHeight;
         cloud.is_dense = false;
-        cloud.points.resize(cloud.width * cloud.height);
+        cloud.points.resize(roiWidth * roiHeight);
         for(size_t i = 0; i < cloud.points.size(); ++i)
         {
-            cloud.points[i].x = xMasked.at<float>(i);
-            cloud.points[i].y = yMasked.at<float>(i);
-            cloud.points[i].z = zMasked.at<float>(i);
+            cloud.points[i].x = x.at<float>(i);
+            cloud.points[i].y = y.at<float>(i);
+            cloud.points[i].z = z.at<float>(i);
             cloud.points[i].r = (uint8_t)rgb.at<cv::Vec3b>(i)[2];
             cloud.points[i].g = (uint8_t)rgb.at<cv::Vec3b>(i)[1];
             cloud.points[i].b = (uint8_t)rgb.at<cv::Vec3b>(i)[0];
@@ -132,11 +172,11 @@ int main()
 
         // Displaying the masked RGB image
         cv::namedWindow("Masked RGB image", cv::WINDOW_AUTOSIZE);
-        cv::imshow("Masked RGB image", rgbMasked);
+        cv::imshow("Masked RGB image", rgb);
         cv::waitKey(0);
 
         // Saving the masked RGB image
-        cv::imwrite("Masked RGB image.jpg", rgbMasked);
+        cv::imwrite("Masked RGB image.jpg", rgb);
     }
     catch(const std::exception &e)
     {
